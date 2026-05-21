925241e44d723dfe816b09b307f3c686991214ca
Juan Arias <juan.fabian.arias@gmail.com>
2026-05-21 18:56:47 -0300

    cuarto commit: finalizacion de la app con el curso

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const ingresos = [
-    new Ingreso ('salario', 500),
-    new Ingreso('Venta de auto', 500)
+    new Ingreso ('Salario', 500),
+    new Ingreso('Venta de auto', 500),
 ];
 
 const egresos = [
@@ -10,6 +10,8 @@ const egresos = [
 
 let cargarApp = ()=>{
     cargarCabecero();
+    cargarIngresos();
+    cargarEgresos();
 }
 
 let totalIngresos = () =>{
@@ -22,7 +24,7 @@ let totalIngresos = () =>{
 
 let totalEgresos = () =>{
     let totalEgreso = 0;
-    for(egreso of egresos){
+    for(let egreso of egresos){
         totalEgreso += egreso.valor;
     }
     return totalEgreso
@@ -32,8 +34,113 @@ let cargarCabecero = () => {
     let presupuesto = totalIngresos() - totalEgresos();
     let porcentajeEgreso = totalEgresos() / totalIngresos();
 
-    document.getElementById('presupuesto').innerHTML = presupuesto;
-    document.getElementById('porcentaje').innerHTML = porcentajeEgreso;
-    document.getElementById('ingresos').innerHTML = totalIngresos();
-    document.getElementById('egresos').innerHTML = totalEgresos();
+    document.getElementById('presupuesto').innerHTML = formatoMoneda(presupuesto);
+    document.getElementById('porcentaje').innerHTML = formatoPorcentaje(porcentajeEgreso);
+    document.getElementById('ingresos').innerHTML = formatoMoneda(totalIngresos());
+    document.getElementById('egresos').innerHTML = formatoMoneda(totalEgresos());
 } 
+
+//esto es para el formato de la moneda
+const formatoMoneda = (valor) =>{
+    return valor.toLocaleString('en-US',{style: 'currency', currency: 'USD',minimumFractionDigits:2});
+}
+
+//formato para el porcentaje
+const formatoPorcentaje = (valor)=>{
+    return valor.toLocaleString('en-US',{style:'percent',minimumFractionDigits:2})
+}
+
+
+//ingresos dinamicos
+const cargarIngresos = () =>{
+    let ingresosHTML = '';
+
+    for(let ingreso of ingresos){
+        ingresosHTML += crearIngresoHTML(ingreso);
+    }
+    document.getElementById('lista-ingresos').innerHTML = ingresosHTML;
+}
+
+const crearIngresoHTML = (ingreso)=>{
+    let ingresoHTML = `
+    <div class="elemento limpiarEstilos">
+                    <div class="elemento_descripcion">${ingreso.descripcion}</div>
+                    <div class="derecha limpiarEstilos">
+                        <div class="elemento_valor">+ ${formatoMoneda(ingreso.valor)}</div>
+                        <div class="elemento_eliminar">
+                            <button class="elemento_eliminar--btn">
+                                <ion-icon name="close-outline"
+                                onclick='eliminarIngreso(${ingreso.id})' ></ion-icon>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+    `;
+    return ingresoHTML;
+}
+
+const eliminarIngreso = (id) =>{
+    let indiceEliminar = ingresos.findIndex( ingreso => ingreso.id === id);
+    ingresos.splice(indiceEliminar, 1);
+    cargarCabecero();
+    cargarIngresos();
+}
+
+
+//egresos dimanicos
+
+const cargarEgresos = () =>{
+    let egresosHTML = '';
+
+    for(let egreso of egresos){
+        egresosHTML += crearEgresoHTML(egreso);
+    }
+
+    document.getElementById('lista-egresos').innerHTML = egresosHTML;
+}
+
+const crearEgresoHTML = (egreso) =>{
+    let egresoHTML = `
+                    <div class="elemento limpiarEstilos">
+                    <div class="elemento_descripcion">${egreso.descripcion}</div>
+                    <div class="derecha limpiarEstilos">
+                        <div class="elemento_valor">- ${formatoMoneda(egreso.valor)}</div>
+                        <div class="elemento_porcentaje">21%</div>
+                        <div class="elemento_eliminar">
+                            <button class="elemento_eliminar--btn">
+                                <ion-icon name="close-outline"
+                                onclick='eliminarEgreso(${egreso.id})'></ion-icon>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+    `;
+    return egresoHTML;
+}
+
+const eliminarEgreso = (id) =>{
+    let indiceEliminar = egresos.findIndex( egreso => egreso.id === id);
+    egresos.splice(indiceEliminar, 1);
+    cargarCabecero();
+    cargarEgresos();
+}
+
+//agregar ingresos y egresos de forma dinamica
+
+let agregarDato = () =>{
+    let forma = document.forms['forma'];
+    let tipo = forma['tipo'];
+    let descripcion = forma ['descripcion'];
+    let valor = forma['valor'];
+    if(descripcion.value !== '' && valor.value !== ''){
+        if(tipo.value === 'ingreso'){
+            ingresos.push(new Ingreso(descripcion.value, Number(valor.value)));
+            cargarCabecero();
+            cargarIngresos();
+        }else if(tipo.value === 'egreso'){
+            egresos.push(new Egreso(descripcion.value, Number(valor.value)));
+            cargarCabecero();
+            cargarEgresos();
+        }
+    }
+}
